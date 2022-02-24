38f86c485ece9d034c02502a411145010e5d80ed
dunghojava <dungho21397@gmail.com>
2022-02-24 08:24:45 +0700

    sửa lại cho dễ nhìn

--- bai4.js
+++ bai4.js
@@ -4,8 +4,8 @@ function nhap() {
     let c = document.getElementById('st3').value;
     document.getElementById('rsa').innerHTML = 'a) Chu vi là: ' + (a * 2) + '<br/>' + 'Diện tích là: ' + (a * a);
     document.getElementById('rsb').innerHTML = 'b) Bình phương bằng: ' + (a * a) + '<br/>' + 'Tích a và b bằng: ' + (a * b);
-
 }
+
 function tongday() {
     let a = +document.getElementById('st1').value;
     let b = +document.getElementById('st2').value;
@@ -15,11 +15,22 @@ function tongday() {
     }
     document.getElementById('rsc').innerHTML = 'c) Tổng là ' + s;
 }
+
+function tongday2() {
+    let a = +document.getElementById('st1').value;
+    let b = +document.getElementById('st2').value;
+    if ((b-a+1)%2 == 0) {
+        document.getElementById('rsc2').innerHTML = 'c) Tổng là ' + ((b+a)*(b-a+1)/2);
+    } else {
+        document.getElementById('rsc2').innerHTML = 'c) Tổng là ' + ((b-1+a)*(b-a)/2+b);
+    }
+}
+
 function theky() {
     let a = +document.getElementById('st1').value;
     document.getElementById('rsd').innerHTML = 'd) Thế kỷ ' + (parseInt(a/100) + 1);
-
 }
+
 function test() {
     let a = +document.getElementById('st1').value;
     let b = +document.getElementById('st2').value;
@@ -35,16 +46,6 @@ function test() {
         if ((a*a) == ((b*b)+(c*c)) || (b*b) == ((a*a)+(c*c)) || (c*c) == ((a*a)+(b*b))) {
             document.getElementById('rseiv').innerHTML = 'eiv) Là 1 tam giác vuông';
         }
-
     }
 }
 
-function tongday2() {
-    let a = +document.getElementById('st1').value;
-    let b = +document.getElementById('st2').value;
-    if ((b-a+1)%2 == 0) {
-        document.getElementById('rsc2').innerHTML = 'c) Tổng là ' + ((b+a)*(b-a+1)/2);
-    } else {
-        document.getElementById('rsc2').innerHTML = 'c) Tổng là ' + ((b-1+a)*(b-a)/2+b);
-    }
-}
